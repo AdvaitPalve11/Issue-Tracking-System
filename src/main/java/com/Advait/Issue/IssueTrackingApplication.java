7a42d3b74af36e35f3abd82832e4fde82b411056
Advait Palve <advaitdpalve@gmail.com>
2026-04-12 21:31:40 +0530

    Refaactor Package to issuue

--- src/main/java/com/Advait/Issue/IssueTrackingApplication.java
+++ src/main/java/com/Advait/Issue/IssueTrackingApplication.java
@@ -1,4 +1,4 @@
-package com.Advait.Issue.Tracking;
+package com.Advait.Issue;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
